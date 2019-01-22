a0969ea9c539e131dbb7601412935d9f0f0328f2
Josiah <josiah.goodson@gmail.com>
2019-01-22 10:21:42 -0500

    Ensure freshest images are used

--- start.sh
+++ start.sh
@@ -8,11 +8,21 @@ docker-compose -f test-docker-compose.yml down
 npm i backend --prefix ./backend/
 npm i frontend --prefix ./frontend/
 
+# Ensure containers are up to date
+docker pull chaffelson/whoville:latest
+josiahgoodson/cloudbreak-cuisine-frontend:latest
+josiahgoodson/cloudbreak-cuisine-backend:latest
+
 # Fix missing Linux package
 docker run -d --name cuisine_frontend -v ${PWD}/frontend:/usr/src/app -t josiahgoodson/cloudbreak-cuisine-frontend:latest
 docker exec cuisine_frontend npm rebuild node-sass
 docker stop cuisine_frontend
 docker rm cuisine_frontend
+
+# Ensure containers are up to date
+docker pull chaffelson/whoville:latest
+josiahgoodson/cloudbreak-cuisine-frontend:latest
+josiahgoodson/cloudbreak-cuisine-backend:latest
 
 # Spin up the containers
 docker-compose -f test-docker-compose.yml up
