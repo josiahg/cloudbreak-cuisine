1eae11fbbc671d73ba977af3723896e0836a272b
Josiah <josiah.goodson@gmail.com>
2019-01-22 10:03:48 -0500

    Updated startup script

--- start.sh
+++ start.sh
@@ -8,5 +8,11 @@ docker-compose -f test-docker-compose.yml down
 npm i backend --prefix ./backend/
 npm i frontend --prefix ./frontend/
 
+# Fix missing Linux package
+docker run -d --name cuisine_frontend -v ${PWD}/frontend:/usr/src/app -t josiahgoodson/cloudbreak-cuisine-frontend:latest
+docker exec cuisine_frontend npm rebuild node-sass
+docker stop cuisine_frontend
+docker rm cuisine_frontend
+
 # Spin up the containers
 docker-compose -f test-docker-compose.yml up
