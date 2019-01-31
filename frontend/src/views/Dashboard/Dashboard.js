78f9865b8427676743a83bcfc0f8e5e74d9cd94d
Paul Vidal <paul.vid@gmail.com>
2019-01-31 14:02:10 -0500

    Minor fix on Dashboard State

--- frontend/src/views/Dashboard/Dashboard.js
+++ frontend/src/views/Dashboard/Dashboard.js
@@ -59,7 +59,11 @@ function DashboardClassName(state) {
 
 function DashboardItemText(props) {
   const dashboardItem = props.dashboardItem
-  const state = dashboardItem.status
+  if (dashboardItem.status.toString() === 'AVAILABLE'){
+    var state = dashboardItem.cluster.status
+  } else {
+    var state =  dashboardItem.status
+  }
   var widget_text = "No creation info"
 
   if (state.toString() === "AVAILABLE") {
